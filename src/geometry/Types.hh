7643075d353c91e8e152bf6032fb819f2af8159e
Guilherme Lima <lima@fnal.gov>
2020-08-27 17:58:10 -0500

    Remove Boundary enum, and replace boundary() with is_outside().

--- src/geometry/Types.hh
+++ src/geometry/Types.hh
@@ -15,16 +15,5 @@ class Geometry;
 //---------------------------------------------------------------------------//
 using VolumeId = OpaqueId<Geometry, unsigned int>;
 
-/*!
- * Whether the particle is inside or outside the problem space.
- *
- * The particle tracking should terminate when the boundary state is "outside".
- */
-enum class Boundary : bool
-{
-    outside = false,
-    inside  = true
-};
-
 //---------------------------------------------------------------------------//
 } // namespace celeritas
